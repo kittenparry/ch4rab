c58ad19dae8e91cac0f81acf11008819b31fa86c
Edvin Boul <kittenparry@users.noreply.github.com>
2022-06-24 15:07:00 +0300

    Grab image by elem ID rather than manual cropping
    Headless browser
    Save to /board/thread/id.png
    Doesn't grab the whole image of the post if it's the OP of the thread, only until end of text, or if no text, end of replies

--- 4chrg.py
+++ 4chrg.py
@@ -1,43 +1,33 @@
 from selenium import webdriver
-import cv2
+from selenium.webdriver.chrome.options import Options
 import os
 
 
-# 4chan and 4channel have different background colours
-# likely will need to crop the image to the borders of these background colours
-
-# 4channel: #d6bad0, #ba9dbf (border)
-# 4chan:    #f0c0b0, #d99f91 (border)
-# as long as it's the default theme, so maybe use a different method?
-
-
 URL = 'https://boards.4channel.org/mu/thread/111250954#p111251146'
+ID = URL.split('#')[-1]                   # p111251146
+THREAD = URL.split('/')[-1].split('#')[0] # 111250954
+BOARD = URL.split('/')[-3]                # mu
 
 RES = 'results'
 if not os.path.isdir(RES):
 	os.mkdir(RES)
 
-TEMP_IMG = os.path.join(RES, 'screen_temp.png')
-END_RES = os.path.join(RES, 'screen.png')
+# results/mu/111250954/
+DEST_DIR = os.path.join(RES, BOARD, THREAD)
+if not os.path.isdir(DEST_DIR):
+	os.makedirs(DEST_DIR)
+
+END_RES = os.path.join(DEST_DIR, f'{ID}.png')
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
 
 def start():
-	browser = webdriver.Chrome()
+	browser = webdriver.Chrome(options=chrome_options)
 	browser.get(URL)
-	browser.save_screenshot(TEMP_IMG)
 
-	img = cv2.imread(TEMP_IMG)
-	y = 0
-	x = 0
-	h = 100
-	w = 200
-	crop_img = img[y: y + h, x: x + w]
-
-	cv2.imwrite(END_RES, crop_img)
-
-
-
-
-
+	element = browser.find_element_by_id(ID)
+	element.screenshot(END_RES)
 
 
 if __name__ == '__main__':
