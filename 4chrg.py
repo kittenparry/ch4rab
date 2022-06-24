996f5641f466e7eac08c54e20e3893c84f027b2a
Edvin Boul <kittenparry@users.noreply.github.com>
2022-06-24 16:03:03 +0300

    Fix checkboxes not working

--- 4chrg.py
+++ 4chrg.py
@@ -15,20 +15,20 @@ class Gui(tk.Frame):
 
 	def add_elements(self):
 		"""Draw GUI elements."""
-		on_image = tk.PhotoImage(width=48, height=24)
-		off_image = tk.PhotoImage(width=48, height=24)
-		on_image.put(("green",), to=(0, 0, 23,23))
-		off_image.put(("red",), to=(24, 0, 47, 23))
+		self.on_image = tk.PhotoImage(width=48, height=24)
+		self.off_image = tk.PhotoImage(width=48, height=24)
+		self.on_image.put(("green",), to=(0, 0, 23, 23))
+		self.off_image.put(("red",), to=(24, 0, 47, 23))
 
-		var1 = tk.IntVar(value=1)
-		var2 = tk.IntVar(value=0)
-		cb1 = tk.Checkbutton(self.main_frame, image=off_image, selectimage=on_image, indicatoron=False,
-			onvalue=1, offvalue=0, variable=var1)
-		cb2 = tk.Checkbutton(self.main_frame, image=off_image, selectimage=on_image, indicatoron=False,
-			onvalue=1, offvalue=0, variable=var2)
+		self.var1 = tk.IntVar(value=1)
+		self.var2 = tk.IntVar(value=0)
+		self.cb1 = tk.Checkbutton(self.main_frame, image=self.off_image, selectimage=self.on_image, indicatoron=False,
+			onvalue=1, offvalue=0, variable=self.var1)
+		self.cb2 = tk.Checkbutton(self.main_frame, image=self.off_image, selectimage=self.on_image, indicatoron=False,
+			onvalue=1, offvalue=0, variable=self.var2)
 
-		cb1.pack(padx=20, pady=10)
-		cb2.pack(padx=20, pady=10)
+		self.cb1.pack(padx=20, pady=10)
+		self.cb2.pack(padx=20, pady=10)
 
 
 
