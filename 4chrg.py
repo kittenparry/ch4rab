6e12ff532bca1afe578dfad45b5a4811ccbefa07
Edvin Boul <kittenparry@users.noreply.github.com>
2022-06-24 18:39:51 +0300

    Merge cad.py into the main program
    It really doesn't need a separate module as it's a small program anyway

--- 4chrg.py
+++ 4chrg.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 import threading
 import traceback
-from cad import check_url
+import os
+import re
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 ON_LBL = 'Listening..'
@@ -117,6 +120,56 @@ class Gui(tk.Frame):
 
 	def update_status(self, text):
 		self.status_label.configure(text=str(text))
+
+# --- beg: cad.py ---
+
+# https://boards.4channel.org/mu/thread/111250954#p111251146
+# 1-4 characters including numbers for 4chan board names
+pattern = 'https:\/\/boards\.(4chan|4channel).org\/[a-zA-Z0-9]{1,4}\/thread\/[0-9]+#p[0-9]+'
+
+def check_url(url):
+	if not url.startswith('https://boards.4chan.org/') and \
+		not url.startswith('https://boards.4channel.org/'):
+		# FIXME: never reaches here, as it gets filtered in the main program already
+		print('Not 4chan URL, terminating.')
+		return 'Not a 4chan URL.'
+
+	print(f'Starting for {url}..')
+
+	if not re.fullmatch(pattern, url):
+		print('Not a reply URL, terminating.')
+		return 'Not a 4chan reply URL.'
+
+	RES = 'results'
+	if not os.path.isdir(RES):
+		os.mkdir(RES)
+
+	# https://boards.4channel.org/mu/thread/111250954#p111251146
+	id = url.split('#')[-1]                   # p111251146
+	thread = url.split('/')[-1].split('#')[0] # 111250954
+	board = url.split('/')[-3]                # mu
+
+	# results/mu/111250954/
+	dest_dir = os.path.join(RES, board, thread)
+	if not os.path.isdir(dest_dir):
+		os.makedirs(dest_dir)
+
+	end_res = os.path.join(dest_dir, f'{id}.png')
+
+	download_panel(url, id, end_res)
+	return f'Complete /{board}/ {id}.png.'
+
+def download_panel(url, id, end_res):
+	chrome_options = Options()
+	chrome_options.add_argument('--headless')
+
+	browser = webdriver.Chrome(options=chrome_options)
+	browser.get(url)
+
+	element = browser.find_element_by_id(id)
+	element.screenshot(end_res)
+
+# --- end: cad.py ---
 
 def get_geometry():
 	"""Return geometry to spawn the program in the middle of the screen.
