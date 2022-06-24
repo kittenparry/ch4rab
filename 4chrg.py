5e4ff139a174b9c15ccf3212d728190d6157d333
Edvin Boul <kittenparry@users.noreply.github.com>
2022-06-24 17:04:21 +0300

    Visual side of things is done
    Two rows, one with toggle button and toggle status label, second with download status label
    Dark theme from the other project
    Remove second checkbox
    Change vars from 0/1 to "Listening.." and "Waiting.."
    Add corresponding label with the same colour next to it, doesn't like the fact it moves the button when changing label as it tries to center, but will look into it later on
    Dummy label for the download status for the moment
    Master grid column configure weight 1 for full width everything, changing window size still centers the elements kind of
    Give a dark background to "off" option, rather than it being invisible
    Change window size to a much smaller one and to the side of the screen under Log Recorder on my setup

--- 4chrg.py
+++ 4chrg.py
@@ -3,36 +3,73 @@ import threading
 import traceback
 
 
+# TODO: downloading ID, complete ID status labels
+
+
 class Gui(tk.Frame):
 
 	def __init__(self, master = None):
 		tk.Frame.__init__(self, master)
-		self.main_frame = tk.Frame(master)
-		self.main_frame.pack()
+		master.grid_columnconfigure(0, weight=1)
+		self.top_frame = tk.Frame(master)
+		self.bot_frame = tk.Frame(master)
+		self.top_frame.grid(row=0, column=0)
+		self.bot_frame.grid(row=1, column=0)
+
+		# Theming™
+		self.colour_scheme('dark')
+		master.configure(bg=self.bg)
+		for frame in [self.top_frame, self.bot_frame]:
+			frame.configure(bg=self.bg)
 
 		self.add_elements()
 
+
+	def colour_scheme(self, mode):
+		"""Define colour scheming for the whole program.
+		
+		Keyword arguments:
+		mode -- selection of dark/light colouring for the program. only dark for now.
+		"""
+		if mode == 'dark':
+			self.bg, self.fg, self.ac1, self.ac2 = ('#282828', '#CAD2C5', '#404040', '#B3B3B3')
+		if mode == 'light':
+			self.bg, self.fg, self.ac1, self.ac2 = ('#FBF8F1', 'black', '#F7ECDE', '#E9DAC1')
 
 	def add_elements(self):
 		"""Draw GUI elements."""
 		self.on_image = tk.PhotoImage(width=48, height=24)
 		self.off_image = tk.PhotoImage(width=48, height=24)
-		self.on_image.put(("green",), to=(0, 0, 23, 23))
-		self.off_image.put(("red",), to=(24, 0, 47, 23))
+		self.on_image.put(('green',), to=(0, 0, 23, 23))
+		self.off_image.put(('red',), to=(24, 0, 47, 23))
 
-		self.var1 = tk.IntVar(value=1)
-		self.var2 = tk.IntVar(value=0)
-		self.cb1 = tk.Checkbutton(self.main_frame, image=self.off_image, selectimage=self.on_image, indicatoron=False,
-			onvalue=1, offvalue=0, variable=self.var1)
-		self.cb2 = tk.Checkbutton(self.main_frame, image=self.off_image, selectimage=self.on_image, indicatoron=False,
-			onvalue=1, offvalue=0, variable=self.var2)
+		self.toggle_is_on = tk.StringVar(value='Listening..')
+		self.toggle_colour = tk.StringVar(value='green')
+		self.toggle_is_on.trace('w', self.update_check_text)
 
-		self.cb1.pack(padx=20, pady=10)
-		self.cb2.pack(padx=20, pady=10)
+		self.checkbox = tk.Checkbutton(self.top_frame, image=self.off_image, selectimage=self.on_image, indicatoron=False,
+			onvalue='Listening..', offvalue='Waiting..', variable=self.toggle_is_on)
+
+		self.check_label = tk.Label(self.top_frame, text=self.toggle_is_on.get())
+
+		self.status_label = tk.Label(self.bot_frame, text='Downloaded 23489234982398.png.')
+
+
+		for el in [self.checkbox, self.check_label, self.status_label]:
+			el.pack(side='left', pady=10, padx=10)
+			el.configure(bg=self.bg, fg=self.fg, borderwidth=0)
+
+		self.check_label.configure(fg=self.toggle_colour.get())
+		self.checkbox.configure(bg=self.ac1)
 
 
 
-
+	def update_check_text(self, *args):
+		if self.toggle_colour.get() == 'red':
+			self.toggle_colour.set('green')
+		else:
+			self.toggle_colour.set('red')
+		self.check_label.configure(text=self.toggle_is_on.get(), fg=self.toggle_colour.get())
 
 
 
@@ -41,10 +78,7 @@ def get_geometry():
 	"""Return geometry to spawn the program in the middle of the screen.
 	Only in 1920px width.
 	"""
-	program_width = 650
-	screen_width = 1920
-	x_position = (screen_width - program_width) / 2
-	return '%dx555+%d+30' % (program_width, x_position)
+	return '250x90+94+145'
 
 def start_gui():
 	"""Launch GUI."""
